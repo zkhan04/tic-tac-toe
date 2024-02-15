1dac5ebb99b4f2310785b4be2e06a450089f0d84
zkhan04 <khanzayd11@gmail.com>
2024-02-14 23:18:58 -0600

    done, I wanna refactor now tho

--- ttt.js
+++ ttt.js
@@ -1,8 +1,11 @@
-gameBoard = (function createGameBoard(){
-    const board = [['', '', ''], ['', '', ''], ['', '', '']];
+const gameBoard = (function createGameBoard(){
+    // state variables
+    let board = [['', '', ''], ['', '', ''], ['', '', '']];
     let gameComplete = false;
     let currentCharacter = 'x';
 
+    // win patterns (there are only 8 so i couldve jus hard coded them
+    // but im a cs major so I need to automate everything LOL)
     const winPatterns = (function generateWinPatterns(){
         const winPatterns = []
 
@@ -33,56 +36,76 @@ gameBoard = (function createGameBoard(){
         return winPatterns;
     })();
     
-    function play(row, column){
+    // plays the selected move and edits the appropriate DOM element
+    function play(event, row, column){
         if (board[row][column] === ''){
+            event.target.textContent = currentCharacter;
             board[row][column] = currentCharacter;
+            checkForWin();
+
+            // switches character
             if (currentCharacter === 'x') currentCharacter = 'o';
             else currentCharacter = 'x';
         } 
-        else console.log('KYS!!');
     }
 
-    function readGameMove(){
-        let [r, c] = prompt(currentCharacter + ": Enter the row and column").split(' ');
-        play(r, c);
-    }
-
-    function runGame(){
-        while (!gameComplete){
-            readGameMove();
-            displayBoard();
-            gameComplete = checkForWin();
-        }
-        console.log(gameComplete + ' wins');
-    }
-
-    function displayBoard(){
-        board.forEach((row) => console.log(row));
-    }
-
+    // checks a given win pattern
     function checkWinPattern(winPattern){
         let [r, c] = winPattern[0];
         let char;
+
+        // returns false if first cell empty
         if (board[r][c] === '') {
             return false;
         } else char = board[r][c];
+
+        // returns false if any cells don't match first one
         for(let i = 1; i < winPattern.length; i++){
             [r, c] = winPattern[i];
             if (board[r][c] != char) {
                 return false;
             } 
         }
-        console.log(winPattern);
-        return char;
     }
 
+    // checks across all win patterns, displays message if a player wins
     function checkForWin(){
         for (let i = 0; i < winPatterns.length; i++){
             const result = checkWinPattern(winPatterns[i]);
-            if (result != false) return result;
+            if (result != false){
+                gameResultField.textContent = currentCharacter + " wins!";
+            }
         }
-        return false;
     }
 
-    return {runGame};
+    // resets the game board and clears the result
+    function reset(){
+        console.log('called');
+        board = Array.from({ length: 3 }, () => Array(3).fill(''));
+        currentCharacter = 'x';
+        Array.from(gameDisplay.getElementsByTagName("*")).forEach((tile) => {
+            tile.textContent = '';
+        })
+        gameResultField.textContent = '';
+    }
+
+    // only public methods
+    return {play, reset};
 })();
+
+// DOM elements
+const gameDisplay = document.querySelector('.game-board');
+const gameResultField = document.querySelector('.result');
+const restartButton = document.querySelector('.restart');
+
+// event listeners
+for (let i = 0; i < 3; i++){
+    for (let j = 0; j < 3; j++){
+        const tile = document.createElement('div');
+        tile.addEventListener('click', (event) => {
+            gameBoard.play(event, i, j);
+        })
+        gameDisplay.appendChild(tile);
+    }
+}
+restartButton.addEventListener('click', (event) => gameBoard.reset());
